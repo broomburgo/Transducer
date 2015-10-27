caee75d1f42fe32bd44b0de1479d25e7fe4d553b
Elviro Rocca <elviro.rocca@facile.it>
2015-10-27 12:23:09 +0100

    Initial Commit

---
+++ Transducer/Transducer.h
@@ -0,0 +1,19 @@
+//
+//  Transducer.h
+//  Transducer
+//
+//  Created by Elviro Rocca on 27/10/15.
+//  Copyright © 2015 Elvi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Transducer.
+FOUNDATION_EXPORT double TransducerVersionNumber;
+
+//! Project version string for Transducer.
+FOUNDATION_EXPORT const unsigned char TransducerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Transducer/PublicHeader.h>
+
+
